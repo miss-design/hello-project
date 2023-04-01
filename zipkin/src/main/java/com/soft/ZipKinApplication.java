ae1c8b37cb80b24ef2e4c71779d0bfc5f9098e80
gitee-pu <2225453885@qq.com>
2023-04-01 18:00:35 +0800

    提交file

---
+++ zipkin/src/main/java/com/soft/ZipKinApplication.java
@@ -0,0 +1,13 @@
+package com.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class,args);
+    }
+}
